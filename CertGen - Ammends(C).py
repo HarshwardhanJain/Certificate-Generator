5e3d685a801bcf51ab21054f56b809a78cfdf43a
HarshwardhanJain <harshwardhan11225@gmail.com>
2024-11-16 13:29:00 +0530

    Corrected the Coordinates - CertGen - Ammends(C).py

--- CertGen - Ammends(C).py
+++ CertGen - Ammends(C).py
@@ -38,8 +38,8 @@ def create_certificates(participants, number, output_dir):
         can.setFont(font, font_size_number)
         
         # Set coordinates for the participant's number
-        x_number = 100  # X-coordinate for the number
-        y_number = 200  # Y-coordinate for the number
+        x_number = 530  # X-coordinate for the number
+        y_number = 190  # Y-coordinate for the number
         
         # Draw the participant's number at the specified coordinates
         can.drawString(x_number, y_number, str(number))
